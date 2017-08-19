84dee2dc154f11e08194a69aa9535daea84fc57d
fabioaromanini <fabioaromanini@gmail.com>
2017-08-18 22:29:01 -0300

    Implementa primeiro tipo de deteccao

--- t1/colisao.c
+++ t1/colisao.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include <limits.h>
 
-/* -----------------------
-__________________________
+/*__________________________
 
 	Minhas Bibliotecas
-__________________________
+__________________________*/
 
- -----------------------*/
 #include <bool.h>
 /* --------------------
 	Tipo bool que representa uma variável
@@ -64,11 +62,19 @@ __________________________
  --------------------*/
 
 // Função que delimita uma caixa reduzida a partir de uma malha de triângulos
-caixa_t *malha_para_caixa(malha_t *malha);
+caixa_t *malha_para_caixa_reduzida(malha_t *malha);
 
 // Função que cria uma caixa reduzida a partir
-// da caixa conforme explicada no pdf
+// de uma caixa semelhante à caixa explicada no pdf
 caixa_t *caixa_para_caixa_reduzida(caixa_t *caixa);
+
+// Função que escolhe um octante em uma caixa,
+// de acordo com a posição de um ponto
+void escolher_octante(caixa_t *caixa, ponto_t ponto);
+
+// Função que determina se um objeto está
+// contido dentro de uma caixa
+bool colisao(malha_t *objeto, caixa_t *caixa, ponto_t colisor, int n);
 
 int main(int argc, char *argv[]) {
 	// Primeiro, obteremos todas as informações
@@ -81,14 +87,15 @@ int main(int argc, char *argv[]) {
 	// printf("%d\n", (int) precisa_fazer_caixa);
 
 	// Numero de iterações para rodar o algoritmo
+	// que reduz a caixa, de acordo com a posição do ponto
 	int n_max;
 	scanf("%d%*c", &n_max);
-	// printf("%d\n", n_max);
+	printf("%d\n", n_max);
 
 	// Número de triângulos que compõem o objeto
 	int n_triangulos;
 	scanf("%d%*c", &n_triangulos);
-	// printf("%d\n", n_triangulos);
+	printf("%d\n", n_triangulos);
 
 	// Coordenadas do ponto colisor:
 	ponto_t* ponto_colisor = ler_pontos(1);
@@ -102,7 +109,7 @@ int main(int argc, char *argv[]) {
 	// ou fazer a caixa, ou ler ela da entrada principal
 	caixa_t *caixa;
 	if(precisa_fazer_caixa)
-		caixa = malha_para_caixa(objeto);
+		caixa = malha_para_caixa_reduzida(objeto);
 	else {
 		ponto_t *pontos_caixa_aux = ler_pontos(8);
 		caixa_t *caixa_aux = nova_caixa(pontos_caixa_aux, 8);
@@ -111,38 +118,19 @@ int main(int argc, char *argv[]) {
 
 		// printf("Caixa real\n");
 		// print_caixa(caixa_aux);
-		
+
 		free_caixa(caixa_aux);
 	}
 
 	// printf("\n\nCaixa fake\n");
 	// print_caixa(caixa);
 
-	// Uma vez que temos os dados necessários, executaremos o 
-	// algoritmo responsável por escolher entre os 8 possíveis
-	// octantes da caixa 
-
-
-	// Começaremos verificando se o ponto colisor
-	// está dentro da caixa delimitadora
-	bool colisao = TRUE;
-	if((*ponto_colisor)[X] > caixa->pontos[MAX][X] ||
-	  (*ponto_colisor)[X] < caixa->pontos[MIN][X] ||
-	  (*ponto_colisor)[Y] > caixa->pontos[MAX][Y] ||
-	  (*ponto_colisor)[Y] < caixa->pontos[MIN][Y] ||
-	  (*ponto_colisor)[Z] > caixa->pontos[MAX][Z] ||
-	  (*ponto_colisor)[Z] < caixa->pontos[MIN][Z])
-		colisao = FALSE;
-
-	// int nivel = 1;
-	// while(nivel <= n_max && colisao) {
-		// Código que escolhe os octantes
-	// }
-
-	if(colisao)
-		printf("1\n");
+	// Uma vez que temos os dados necessários, executaremos
+	// o que detecta se houve colisão ou não.
+	if(colisao(objeto, caixa, *ponto_colisor, n_max))
+		printf("1 - RODO CERTIN\n");
 	else
-		printf("0\n");
+		printf("0 - FAIO\n");
 
 	free_malha(objeto);
 	free_caixa(caixa);
@@ -152,7 +140,7 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-caixa_t *malha_para_caixa(malha_t *m) {
+caixa_t *malha_para_caixa_reduzida(malha_t *m) {
 	// Começamos encontrando os limites do objeto em cada eixo
 	double max_x = INT_MIN;
 	double max_y = INT_MIN;
@@ -234,4 +222,73 @@ caixa_t *caixa_para_caixa_reduzida(caixa_t *caixa) {
 	pontos_caixa[MIN][Z] = min_z;
 
 	return nova_caixa(pontos_caixa, N_PONTOS);
+}
+
+bool colisao(malha_t *objeto, caixa_t *caixa, ponto_t ponto_colisor, int n) {
+	// Primeiro, checaremos se o ponto 
+	// colisor está nos limites do objeto.
+	if(ponto_colisor[X] < caixa->pontos[MIN][X] ||
+	   ponto_colisor[Y] < caixa->pontos[MIN][Y] ||
+	   ponto_colisor[Z] < caixa->pontos[MIN][Z] ||
+	   ponto_colisor[X] > caixa->pontos[MAX][X] ||
+	   ponto_colisor[Y] > caixa->pontos[MAX][Y] ||
+	   ponto_colisor[Z] > caixa->pontos[MAX][Z]) {
+		printf("Ponto fora da caixa inicial!\n");
+		return TRUE;
+	}
+
+	// Aqui optei por reduzir a caixa ao máximo antes de checar
+	// se houve colisão, pois isso torna o algoritmo menos custoso
+	// quando analisamos o pior caso.
+	for(int i = 0; i < n; i++)
+		escolher_octante(caixa, ponto_colisor);
+
+	// Agora, checaremos se o objeto intercepta o octante final.
+	for(int i = 0; i < objeto->n_triangulos; i++) {
+		for(int j = 0; j < 3; j++) {
+			// Primeiro, checaremos os vértices de todos os triangulos
+			if(objeto->triangulos[i][j][X] >= caixa->pontos[MIN][X] && objeto->triangulos[i][j][X] >= caixa->pontos[MIN][X] &&
+			   objeto->triangulos[i][j][Y] >= caixa->pontos[MIN][Y] && objeto->triangulos[i][j][Y] >= caixa->pontos[MIN][Y] &&
+			   objeto->triangulos[i][j][Z] >= caixa->pontos[MIN][Z] && objeto->triangulos[i][j][Z] >= caixa->pontos[MIN][Z]) {
+				printf("INTERCEPTOU!\n");
+				printf("triangulo %d vertice %d\n", i, j);
+				print_ponto(objeto->triangulos[i][j]);
+				return TRUE;
+			}
+
+		}
+	}
+
+	return TRUE;
+}
+
+void escolher_octante(caixa_t *caixa, ponto_t ponto) {
+	// Substituiremos as coordenadas antigas da caixa com as novas
+	// coordenadas encontradas.Isso se deve à escolha de não
+	// manter um log de caixas, visto que não tem nenhuma utilidade
+	// para esse trabalho. No entanto, imutabilidade de objetos
+	// é tido como uma boa prática, uma vez que é possível manter
+	// um histórico, e o uso de memória não é tão elevado ( O(n_max) )
+
+	// Como temos garantido que o ponto está
+	// dentro da caixa, os ifs ficam bem simples
+	double meio = (caixa->pontos[MAX][X] + caixa->pontos[MIN][X]) / 2;
+	if(ponto[X] >= meio)
+		caixa->pontos[MIN][X] = meio;
+	else
+		caixa->pontos[MAX][X] = meio;
+
+	meio = (caixa->pontos[MAX][Y] + caixa->pontos[MIN][Y]) / 2;
+	if(ponto[Y] >= meio)
+		caixa->pontos[MIN][Y] = meio;
+	else
+		caixa->pontos[MAX][Y] = meio;
+
+	meio = (caixa->pontos[MAX][Z] + caixa->pontos[MIN][Z]) / 2;
+	if(ponto[Z] >= meio)
+		caixa->pontos[MIN][Z] = meio;
+	else
+		caixa->pontos[MAX][Z] = meio;
+
+	print_caixa(caixa);
 }
